302e4fbcfab60d333899a8c5ac40bfcc129aed92
ouioui <adam.larkem@epitech.eu>
2025-04-29 16:33:24 +0200

    feat: message de l'ia

--- llm_deepseek.py
+++ llm_deepseek.py
@@ -1,10 +1,7 @@
-#!/bin/python3
-
 from transformers import pipeline
 from ollama import chat
 import re
 
-# Appel à Ollama
 def Call_Ollama(text):
     response = chat(
         model='deepseek-r1:7b',
@@ -15,36 +12,49 @@ def Call_Ollama(text):
     print(clean_output.strip())
     return clean_output.strip()
 
-# Initialiser la pipeline avec un modèle multilingue
 classifier = pipeline(
     "zero-shot-classification",
     model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli"
 )
 
-# Définir les catégories à détecter
 labels = [
-    "remerciement"
+    "code informatique",
+    "chercharble sur google facilement",
+    "bibliothèque standard C",
 ]
 
 
 def classify_prompt(text):
     result = classifier(text, candidate_labels=labels, multi_label=True)
-    # print(f"Texte : {text}\n")
-    # print("Classification :")
-    for label, score in zip(result["labels"], result["scores"]):
-        if score > 0.7:
-            print(f" - {label}: {score:.2f}")
-            return False 
-    return True
+    scores = dict(zip(result["labels"], result["scores"]))
 
-# Exemple d'utilisation
+    code_score = scores.get("code informatique", 0)
+    google_score = scores.get("cherchable sur google facilement", 0)
+    lib_score = scores.get("bibliothèque standard C", 0)
+
+    if code_score > 0.7:
+        print("code informatique")
+        if lib_score > 0.3:
+            print("bibliothèque standard C")
+            return True, "renvoyer vers man"
+        elif google_score > 0.8:
+            print("cherchable sur google facilement")
+            return True, "renvoyer vers Google"
+        else:
+            print("code informatique")
+            return True, "réponse correcte (code informatique)"
+    else:
+        print("fakse")
+        return False, "réponse incorrecte"
+
 if __name__ == "__main__":
     text = ""
     call_api = False
     while (text != "exit"):
         text = input("=> ")
         call_api = classify_prompt(text)
-        if call_api == True:
+        print(call_api)
+        if call_api[0] == True:
             Call_Ollama(text)
         else:
             print("Message pas conforme")
